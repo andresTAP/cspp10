7dbbc190ffa71654b50dd15317b43e14015cce12
Andres Tappy <andrestapia@uagateway.org>
2016-11-17 17:17:04 +0000

    Unit 4 LAb

--- Unit 4/AT.rps.py
+++ Unit 4/AT.rps.py
@@ -1,36 +1,32 @@
-import random 
- 
-player = input("Enter your choice (rock|paper|scissors): ");
-player = player.lower();
-while (player != "rock" and player != "paper" and player != "scissors"):
-	print(player);
-	player = input("That choice is not valid. Enter your choice (rock/paper/scissors): ");
-	player = player.lower();
-computerInt = random.randint(0,2);
-if (computerInt == 0):
-	computer = "rock";
-elif (computerInt == 1):
-	computer = "paper";
-elif (computerInt == 2):
-	computer = "scissors";
-else:
-	computer = "Huh? Error...";
-if (player == computer):
-	print("Draw!");
-elif (player == "rock"):
-	if (computer == "paper"):
-		print("Computer wins!");
-	else:
-		print("You win!");
-elif (player == "paper"):
-	if (computer == "rock"):
-		print("You win!");
-	else:
-		print("Computer wins!")
-elif (player == "scissors"):
-	if (computer == "rock"):
-		print("Computer wins!");
-	else:
-		print("You win!");
-		print("Your choice: " + player + "\nComputer choice: " + computer + "\nThank you for playing!");
-input("Enter any key to exit.");
+import random
+
+
+
+
+
+
+def get_p1_move():
+    p1_move = input("Would you like to play Rock, Paper or Scissors ")
+    if p1_move.lower() == "rock":
+        return "Rock"
+    elif p1_move.lower() == "paper":
+        return "Paper"
+    elif p1_move.lower() == "scissors":
+        return "Scissors"
+def get_comp_move():
+    comp_move = random.randint(1,3)
+    if comp_move == 1:
+        return "Rock"
+    elif comp_move == 2:
+        return "Paper"
+    elif comp_move == 3:
+        return "Scissors"
+def get_round():
+    rounds = int(input("how many rounds would you like to play from 1 to 9 "))
+    return rounds
+
+    
+    
+print(get_comp_move())
+print(get_p1_move())
+print(get_round())
