7b67b55c94a3ba6fa07c40ce4066c28abe8edb95
Andres Tappy <andrestapia@uagateway.org>
2016-12-08 17:17:17 +0000

    Bhesgusbchjsbvjdz

--- Unit 4/AT.Craps.py
+++ Unit 4/AT.Craps.py
@@ -1,17 +1,17 @@
 import random
+The_House = 100
 player_amount = 100
 
 
 print("Lets Play some Craps!")
+print("----------------------")
 
+def phase_1():
+    if player_amount < 0:
+        return " You are broke!"
+    elif player_amount > 0:
+        return input(int(" How much would you like to bet?: "))
 
-
-
-
-#function name: Rolling dice
-#purpose: generating two dice rolls and pritns it out, and returns the sum
-#arguements:none
-#returns: teh sum of two dice
 def rolling_dices():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
@@ -19,4 +19,15 @@ def rolling_dices():
     print("Rolled 2 dice: {} {}".format(dice1,dice2))
     return dice_sum
 
-rolling_dices()
+def phase_2(sum_of_dice):
+    if sum_of_dice == (7): 
+        return "Player wins!"
+    elif sum_of_dice == (2,3,12): 
+       return "House Wins!"
+    else: 
+        return "point"
+
+def phase_3():    
+    
+    
+#rolling_dices() 
