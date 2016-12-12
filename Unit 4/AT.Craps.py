8937b9d7ddb1395a6fa29aedda8a93217f35352c
Andres Tappy <andrestapia@uagateway.org>
2016-12-12 17:16:03 +0000

    Crap games

--- Unit 4/AT.Craps.py
+++ Unit 4/AT.Craps.py
@@ -1,16 +1,22 @@
 import random
-The_House = 100
 player_amount = 100
 
 
 print("Lets Play some Craps!")
 print("----------------------")
 
-def phase_1():
+def player_regulations():
     if player_amount < 0:
         return " You are broke!"
     elif player_amount > 0:
         return input(int(" How much would you like to bet?: "))
+
+def bet_amount(player_bet):
+    if player_bet 
+
+def phase_1():
+    if dice_sum == (7) and (11):
+        return player_bet 
 
 def rolling_dices():
     dice1 = random.randint(1,6)
@@ -20,14 +26,19 @@ def rolling_dices():
     return dice_sum
 
 def phase_2(sum_of_dice):
-    if sum_of_dice == (7): 
-        return "Player wins!"
-    elif sum_of_dice == (2,3,12): 
-       return "House Wins!"
+    if sum_of_dice == (7) or (11): 
+        return "Player wins!" +1
+    elif sum_of_dice == (2) or (3) and (12): 
+       return "House Wins!" +1
     else: 
         return "point"
 
-def phase_3():    
+def phase_3():
+    if dice_sum == 
     
-    
-#rolling_dices() 
+
+
+#player_regulations()
+#phase_1()
+#rolling_dices()
+#phase_2()
